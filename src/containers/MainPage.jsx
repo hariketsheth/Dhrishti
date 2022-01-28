c8182a806db19f5bb95ea9536a1725d1d0bfb287
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-28 21:03:40 +0530

    Fixed `Components`

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -32,4 +32,5 @@ const MainPageContainer = () => {
     </div>
   )
 }
+
 export default MainPageContainer;
--- src/containers/OnboardingPage.jsx
+++ src/containers/OnboardingPage.jsx
@@ -8,4 +8,5 @@ const OnboardingPageContainer = () => {
     </div>
   )
 }
+
 export default OnboardingPageContainer;
