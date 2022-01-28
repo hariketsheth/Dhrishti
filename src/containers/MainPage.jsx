b5b63e5a01f7cdfd6280a28fbf53054e5ef8ef5a
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-28 21:37:47 +0530

    Added `SpeechDetector`

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,36 +1,11 @@
-import React, { useState } from 'react';
-
-import ParentPage from '../components/ParentPage';
-import SideBar from '../components/SideBar';
+import React from 'react';
+import SpeechDetector from '../components/SpeechDetector';
+import Video from '../components/Video';
 
 const MainPageContainer = () => {
-
-  const [click, setClick] = useState('steps');
-
   return (
     <div>
-      <SideBar click={click} 
-        setClick={(clickInput) => {
-          setClick(clickInput);
-        }}
-      />
-      {
-        click === 'camera' ?
-          <ParentPage>
-            <div>1</div>
-          </ParentPage> 
-        : click === 'bullet' ?
-          <ParentPage>
-            <div>2</div>
-          </ParentPage> 
-        : click === 'location' ?
-          <ParentPage>
-            <div>3</div>
-          </ParentPage> :
-        <></>
-      }
+      <SpeechDetector />
     </div>
   )
 }
-
-export default MainPageContainer;
