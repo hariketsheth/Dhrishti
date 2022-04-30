c9925a054a4e0af9adc219b43a0f217d54fe1ad3
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-04-30 22:19:43 +0530

    Added `Results` Page and `utils`

---
+++ frontend/src/components/HomePage/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { Button } from 'antd';
+import logo from '../../assets/logo.svg';
+import demo from '../../assets/demo.svg';
+import blueglow from '../../assets/blueglow.png';
+import righthome from '../../assets/righthome.png';
+
+import './style.scss';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <img src={righthome} alt="pill" className="righthome" />
+      <img src={blueglow} alt="pill" className="blueglow" />
+      <div className="home-nav">
+        <img src={logo} alt="logo" className="logo" />
+        <div className="links">
+          <NavLink to="/">
+            <h4>Features</h4>
+          </NavLink>
+          <NavLink to="/">
+            <h4>Pricing</h4>
+          </NavLink>
+          <NavLink to="/">
+            <h4>About</h4>
+          </NavLink>
+          <NavLink to="/">
+            <h4>Team</h4>
+          </NavLink>
+        </div>
+        <NavLink to="/onboarding">
+          <img src={demo} alt="demo" className="demo-button" />
+        </NavLink>
+      </div>
+      <div className="content">
+        <h1>Re-imagine Health</h1>
+        <p>
+          Swaasthaya allows health care professionals to evaluate, diagnose
+          and treat patients at a distance using technology.
+          The approach has been through a striking evolution in the last decade.
+        </p>
+        <div className="buttons">
+          <NavLink to="/onboarding">
+            <Button className="begin" size="large">
+              Begin Now
+            </Button>
+          </NavLink>
+          <NavLink to="/onboarding">
+            <Button className="view" size="large">
+              View Info
+            </Button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/containers/ResultPage.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import HeartRateGraph from '../components/HeartRateGraph';
+import BarGraph from '../components/BarGraph';
+import ProgressGraph from '../components/ProgressGraph';
+import PrognosisChart from '../components/PrognosisChart';
+
+const checkYesOrNo = (string) => {
+    const yesPatterns = ["yes","yeah","yep","sometimes", "occasionally", "do", "think so"];
+    const noPatterns = ["no", "nope", "don't think so", "haven't", "have not", "doesn't"];
+    const patternMatchReduce = (acc, pattern) => acc || (string.match(pattern) !== null);
+    const mightHaveYes = yesPatterns.reduce(patternMatchReduce, false);
+    const mightHaveNo = noPatterns.reduce(patternMatchReduce, false);
+
+    // give more precedence to nos than yes's (if mightHaveNo is true then the entire thing is false)
+    return (!mightHaveNo) && mightHaveYes;
+}
+
+const ResultPageContainer = (props) => {
+  const [mentalHealthScore, setMentalHealthScore] = useState(0);
+  const [overallHealthScore, setOverallHealthScore] = useState(0);
+  const [physicalHealthScore, setPhysicalHealthScore] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {setMentalHealthScore(89);
+    setOverallHealthScore(90);
+    setPhysicalHealthScore(91);
+    }, 2000);
+  }, [])
+
+  const [newEmotionData, setEmotion] = useState([['name1','318'],['name2','93'],['name3','43'],['name4','20'],['name5','15']]);
+
+  useEffect(() => {
+    console.log(props.emotionData);
+    console.log('feels');
+    let sortable = [];
+    for (let emotion in props.emotionData) {
+        sortable.push([emotion, props.emotionData[emotion]]);
+    }
+
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    console.log(sortable);
+    setEmotion(sortable.slice(0, 5));
+
+  }, [props.emotionData]);
+
+  return (
+    <>
+    <div className="result-page">
+      <ProgressGraph color={{
+        '0%': '#A643F4',
+        '100%': 'rgba(249, 89, 166, 0.9)',
+        }}
+        description="Based off of 3 heuristics"
+        name="&#x1F4AF; Overall Score"
+        percent={overallHealthScore}
+        title="Total Checkup"/>
+      <ProgressGraph color={{
+        '0%': '#AE72FF',
+        '100%': '#9FC0FA',
+        }}
+        description="Based off your audio answers"
+        name="&#x2695; Mental Score"
+        percent={mentalHealthScore}
+        title="Mental Health"/>
+      <ProgressGraph color={{
+        '0%': '#FA5656',
+        '100%': '#DFA1CE',
+        }}
+        description="Based off your audio answers"
+        name="&#x1F4AA; Physical Score"
+        percent={physicalHealthScore}
+        title="Physical Health"/>
+      <HeartRateGraph data={props.heartData} />
+      <BarGraph emotions={newEmotionData}/>
+    </div>
+          <PrognosisChart progonsis={[['Inconsistent Heart Rate','32'], ['Acne','14'],['Nearsightedness','4'],['Farsightedness','2'], ['Stress','1']]}/>
+    </>
+  )
+}
+
+export default ResultPageContainer;
